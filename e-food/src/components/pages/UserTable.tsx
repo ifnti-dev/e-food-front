535dd1383abc6bfdfde940b7c320c6c602c99c88
fayssol <fayssologbone@gmail.com>
2024-04-12 10:14:50 +0000

    ...

--- e-food/src/components/pages/UserTable.tsx
+++ e-food/src/components/pages/UserTable.tsx
@@ -9,7 +9,7 @@ const data = [
     adress: "$320,800",
     contact: "2011/04/25",
     status: "Edinburgh",
-    actions: "5421",
+    action: "5421",
   },
   {
     id: "2",
@@ -18,7 +18,7 @@ const data = [
     adress: "$320,800",
     contact: "2011/04/25",
     status: "Edinburgh",
-    actions: "5421",
+    action: "5421",
   },
 ];
 
@@ -28,11 +28,25 @@ const columns = [
   { data: "adress", title: "Adresse" },
   { data: "contact", title: "Contacts" },
   { data: "status", title: "Status" },
-  { data: "contact", title: "Actions" },
+  { data: "action", title: "Actions" },
 ];
 
 const UserTable = () => {
-  return <ReactDataTable data={data} columns={columns} />;
+  return(
+    <>
+        <div className="card mb-3">
+            <div className="card-header py-3  bg-transparent border-bottom-0 d-flex justify-content-between">
+                <h6 className="mb-0 fw-bold">Liste Users</h6>
+                <button type="button" className="btn btn-primary"><i className="bi bi-plus-lg"></i>Ajouter</button>
+            </div>
+            <div className="card-body">
+           
+            <ReactDataTable data={data} columns={columns} />
+            </div>
+        </div>
+    </>
+  ) 
+ 
 };
 
 export default UserTable;
