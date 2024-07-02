6a4ac4eb3497044194f48591e81e65fd8a09010b
fideleChester <ekloufidele82@gmail.com>
2024-07-02 10:51:03 +0000

    change token

--- e-food/src/components/utils/axios.ts
+++ e-food/src/components/utils/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
     baseURL: 'http://localhost:8085/e-food/',
     timeout: 1000,
-    headers: {'X-Custom-Header': 'foobar','Authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmaWZpIiwiaWF0IjoxNzE5OTE2NTQxLCJleHAiOjE3MTk5MTY5MDF9.6sTgNSC-hGzPCln-0f83YYR-NSL123_68MT5hepwRBI'}
+    headers: {'X-Custom-Header': 'foobar','Authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmaWZpIiwiaWF0IjoxNzE5OTE3NDAwLCJleHAiOjE3MTk5MTc3NjB9.eiFAak3WsB9B1jVrEPOEpuH8VvYzphWHcaGw8OI_NVo'}
   });
 
 
